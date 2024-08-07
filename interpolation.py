d2430bef2bc090193e5939fe865dabd3c11e030c
sivansenvetu <sivan0611998@gmail.com>
2024-08-07 23:49:47 +0300

    hw4

---
+++ interpolation.py
@@ -0,0 +1,387 @@
+import numpy as np
+
+
+def linearInterpolation(table_points, point):
+    p = []
+    result = 0
+    flag = 1
+    for i in range(len(table_points)):
+        p.append(table_points[i][0])
+    for i in range(len(p) - 1):
+        if i <= point <= i + 1:
+            x1 = table_points[i][0]
+            x2 = table_points[i + 1][0]
+            y1 = table_points[i][1]
+            y2 = table_points[i + 1][1]
+            result = (((y1 - y2) / (x1 - x2)) * point) + ((y2 * x1) - (y1 * x2)) / (x1 - x2)
+            print("\nThe approximation (interpolation) of the point ", point, " is: ", round(result, 4))
+            flag = 0
+    if flag:
+        x1 = table_points[0][0]
+        x2 = table_points[1][0]
+        y1 = table_points[0][1]
+        y2 = table_points[1][1]
+        m = (y1 - y2) / (x1 - x2)
+        result = y1 + m * (point - x1)
+        print("\nThe approximation (extrapolation) of the point ", point, " is: ", round(result, 4))
+
+
+def run_linearInterpolation():
+    table_points = [(0, 0), (1, 0.8415), (2, 0.9093), (3, 0.1411), (4, -0.7568), (5, -0.9589), (6, -0.2794)]
+    x = 1.28
+    print("Linear Interpolation:\n")
+    print("Table Points: ", table_points)
+    print("Finding an approximation to the point: ", x)
+    linearInterpolation(table_points, x)
+
+
+def RowXchange(matrix, vector):
+    """
+    Function for replacing rows with both a matrix and a vector
+    :param matrix: Matrix nxn
+    :param vector: Vector n
+    :return: Replace rows after a pivoting process
+    """
+
+    for i in range(len(matrix)):
+        max = abs(matrix[i][i])
+        for j in range(i, len(matrix)):
+            # The pivot member is the maximum in each column
+            if abs(matrix[j][i]) > max:
+                temp = matrix[j]
+                temp_b = vector[j]
+                matrix[j] = matrix[i]
+                vector[j] = vector[i]
+                matrix[i] = temp
+                vector[i] = temp_b
+                max = abs(matrix[i][i])
+
+    return [matrix, vector]
+
+
+def Determinant(matrix, mul):
+    """
+    Recursive function for determinant calculation
+    :param matrix: Matrix nxn
+    :param mul: The double number
+    :return: determinant of matrix
+    """
+    width = len(matrix)
+    # Stop Conditions
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        det = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            # Change the sign of the multiply number
+            sign *= -1
+            #  Recursive call for determinant calculation
+            det = det + mul * Determinant(m, sign * matrix[0][i])
+    return det
+
+
+def MakeIMatrix(cols, rows):
+    # Initialize a identity matrix
+    return [[1 if x == y else 0 for y in range(cols)] for x in range(rows)]
+
+
+def MulMatrixVector(InversedMat, b_vector):
+    """
+    Function for multiplying a vector matrix
+    :param InversedMat: Matrix nxn
+    :param b_vector: Vector n
+    :return: Result vector
+    """
+    result = []
+    # Initialize the x vector
+    for i in range(len(b_vector)):
+        result.append([])
+        result[i].append(0)
+    # Multiplication of inverse matrix in the result vector
+    for i in range(len(InversedMat)):
+        for k in range(len(b_vector)):
+            result[i][0] += InversedMat[i][k] * b_vector[k][0]
+    return result
+
+
+def MultiplyMatrix(matrixA, matrixB):
+    """
+    Function for multiplying 2 matrices
+    :param matrixA: Matrix nxn
+    :param matrixB: Matrix nxn
+    :return: Multiplication between 2 matrices
+    """
+    # result matrix initialized as singularity matrix
+    result = [[0 for y in range(len(matrixB[0]))] for x in range(len(matrixA))]
+    for i in range(len(matrixA)):
+        # iterate through columns of Y
+        for j in range(len(matrixB[0])):
+            # iterate through rows of Y
+            for k in range(len(matrixB)):
+                result[i][j] += matrixA[i][k] * matrixB[k][j]
+    return result
+
+
+def InverseMatrix(matrix,vector):
+    """
+    Function for calculating an inverse matrix
+    :param matrix:  Matrix nxn
+    :return: Inverse matrix
+    """
+    # Unveri reversible matrix
+    if Determinant(matrix, 1) == 0:
+        print("Error,Singular Matrix\n")
+        return
+    # result matrix initialized as singularity matrix
+    result = MakeIMatrix(len(matrix), len(matrix))
+    # loop for each row
+    for i in range(len(matrix[0])):
+        # turn the pivot into 1 (make elementary matrix and multiply with the result matrix )
+        # pivoting process
+        matrix, vector = RowXchange(matrix, vector)
+        elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+        elementary[i][i] = 1/matrix[i][i]
+        result = MultiplyMatrix(elementary, result)
+        matrix = MultiplyMatrix(elementary, matrix)
+        # make elementary loop to iterate for each row and subtracrt the number below (specific) pivot to zero  (make
+        # elementary matrix and multiply with the result matrix )
+        for j in range(i+1, len(matrix)):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+
+    # after finishing with the lower part of the matrix subtract the numbers above the pivot with elementary for loop
+    # (make elementary matrix and multiply with the result matrix )
+    for i in range(len(matrix[0])-1, 0, -1):
+        for j in range(i-1, -1, -1):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            elementary[j][i] = -(matrix[j][i])
+            matrix = MultiplyMatrix(elementary, matrix)
+            result = MultiplyMatrix(elementary, result)
+
+    return result
+
+
+def GaussJordanElimination(matrix, vector):
+    """
+    Function for solving a linear equation using gauss's elimination method
+    :param matrix: Matrix nxn
+    :param vector: Vector n
+    :return: Solve Ax=b -> x=A(-1)b
+    """
+    # Pivoting process
+    matrix, vector = RowXchange(matrix, vector)
+    # Inverse matrix calculation
+    invert = InverseMatrix(matrix, vector)
+    return MulMatrixVector(invert, vector)
+
+
+def RowXchageZero(matrix,vector):
+    """
+      Function for replacing rows with both a matrix and a vector
+      :param matrix: Matrix nxn
+      :param vector: Vector n
+      :return: Replace rows after a pivoting process
+      """
+
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            # The pivot member is not zero
+            if matrix[i][i] == 0:
+                temp = matrix[j]
+                temp_b = vector[j]
+                matrix[j] = matrix[i]
+                vector[j] = vector[i]
+                matrix[i] = temp
+                vector[i] = temp_b
+
+    return [matrix, vector]
+
+
+def MaxNorm(matrix):
+    """
+    Function for calculating the max-norm of a matrix
+    :param matrix: Matrix nxn
+    :return:max-norm of a matrix
+    """
+    max_norm = 0
+    for i in range(len(matrix)):
+        norm = 0
+        for j in range(len(matrix)):
+            # Sum of organs per line with absolute value
+            norm += abs(matrix[i][j])
+        # Maximum row amount
+        if norm > max_norm:
+            max_norm = norm
+
+    return max_norm
+
+
+def Cond(matrix, invert):
+    """
+    :param matrix: Matrix nxn
+    :param invert: Inverted matrix
+    :return: CondA = ||A|| * ||A(-1)||
+    """
+    print("|| A ||max = ", MaxNorm(matrix))
+    print("|| A(-1) ||max = ", MaxNorm(invert))
+    return MaxNorm(matrix)*MaxNorm(invert)
+
+
+def UMatrix(matrix,vector):
+    """
+    :param matrix: Matrix nxn
+    :return:Disassembly into a  U matrix
+    """
+    # result matrix initialized as singularity matrix
+    U = MakeIMatrix(len(matrix), len(matrix))
+    # loop for each row
+    for i in range(len(matrix[0])):
+        # pivoting process
+        matrix, vector = RowXchageZero(matrix, vector)
+        for j in range(i + 1, len(matrix)):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            # Finding the M(ij) to reset the organs under the pivot
+            elementary[j][i] = -(matrix[j][i])/matrix[i][i]
+            matrix = MultiplyMatrix(elementary, matrix)
+    # U matrix is a doubling of elementary matrices that we used to reset organs under the pivot
+    U = MultiplyMatrix(U, matrix)
+    return U
+
+
+def LMatrix(matrix, vector):
+    """
+       :param matrix: Matrix nxn
+       :return:Disassembly into a  L matrix
+       """
+    # Initialize the result matrix
+    L = MakeIMatrix(len(matrix), len(matrix))
+    # loop for each row
+    for i in range(len(matrix[0])):
+        # pivoting process
+        matrix, vector = RowXchageZero(matrix, vector)
+        for j in range(i + 1, len(matrix)):
+            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
+            # Finding the M(ij) to reset the organs under the pivot
+            elementary[j][i] = -(matrix[j][i])/matrix[i][i]
+            # L matrix is a doubling of inverse elementary matrices
+            L[j][i] = (matrix[j][i]) / matrix[i][i]
+            matrix = MultiplyMatrix(elementary, matrix)
+
+    return L
+
+
+def SolveLU(matrix, vector):
+    """
+    Function for deconstructing a linear equation by ungrouping LU
+    :param matrix: Matrix nxn
+    :param vector: Vector n
+    :return: Solve Ax=b -> x=U(-1)L(-1)b
+    """
+    matrixU = UMatrix(matrix)
+    matrixL = LMatrix(matrix)
+    return MultiplyMatrix(InverseMatrix(matrixU), MultiplyMatrix(InverseMatrix(matrixL), vector))
+
+
+def solveMatrix(matrixA,vectorb):
+    detA = Determinant(matrixA, 1)
+    print("\nDET(A) = ", detA)
+
+    if detA != 0:
+        print("CondA = ", Cond(matrixA, InverseMatrix(matrixA, vectorb)))
+        print("\nnon-Singular Matrix - Perform GaussJordanElimination")
+        result = GaussJordanElimination(matrixA, vectorb)
+        print(np.array(result))
+        return result
+    else:
+        print("Singular Matrix - Perform LU Decomposition\n")
+        print("Matrix U: \n")
+        print(np.array(UMatrix(matrixA, vectorb)))
+        print("\nMatrix L: \n")
+        print(np.array(LMatrix(matrixA, vectorb)))
+        print("\nMatrix A=LU: \n")
+        result = MultiplyMatrix(LMatrix(matrixA, vectorb), UMatrix(matrixA, vectorb))
+        print(np.array(result))
+        return result
+
+
+def polynomialInterpolation(table_points, x):
+    matrix = [[point[0] ** i for i in range(len(table_points))] for point in table_points] # Makes the initial matrix
+
+    b = [[point[1]] for point in table_points]
+
+    print("The matrix obtained from the points: \n", np.array(matrix))
+    print("\nb vector: \n", np.array(b))
+    matrixSol = solveMatrix(matrix, b)
+
+    result = sum([matrixSol[i][0] * (x ** i) for i in range(len(matrixSol))])
+    print("\nThe polynom: ")
+    print('P(X) = '+'+'.join([ '('+str(matrixSol[i][0])+') * x^' + str(i) + ' ' for i in range(len(matrixSol))])  )
+    print(f"\nThe Result of P(X={x}) is:")
+    print(result)
+    return result
+
+
+def run_polynomialInterpolation():
+    table_points = [(0, 0, 0), (1, 0.8415, 0.914), (2, 0.9093, 1.2), (3, 0.5789, 0.1411), (4, 0.4751, -0.7568), (5, 2.5123, -0.9589), (6, 1.2879, -0.2794)]
+    x = 1.28
+    print("Polynomial Interpolation:\n")
+    print("Table Points: ", table_points)
+    print("Finding an approximation to the point: ", x, '\n')
+    polynomialInterpolation(table_points, x)
+
+
+def lagrange_interpolation(x_data, y_data, x):
+    """
+    Lagrange Interpolation
+
+    Parameters:
+    x_data (list): List of x-values for data points.
+    y_data (list): List of y-values for data points.
+    x (float): The x-value where you want to evaluate the interpolated polynomial.
+
+    Returns:
+    float: The interpolated y-value at the given x.
+    """
+    n = len(x_data)
+    result = 0.0
+
+    for i in range(n):
+        term = y_data[i]
+        for j in range(n):
+            if i != j:
+                term *= (x - x_data[j]) / (x_data[i] - x_data[j])
+        result += term
+
+    return result
+
+
+def run_lagrange_interpolation():
+    print("Lagrange Interpolation:\n")
+    x_data = [1, 2, 5]
+    y_data = [1, 0, 2]
+    x_interpolate = 3  # The x-value where you want to interpolate
+    y_interpolate = lagrange_interpolation(x_data, y_data, x_interpolate)
+    print("Interpolated value at x =", x_interpolate, "is y =", y_interpolate)
+
+
+if __name__ == '__main__':
+    choose = int(input("Choose:\n1 - linear Interpolation\n2 - Polynomial Interpolation\n3 - lagrange Interpolation\n"))
+    match choose:
+        case 1:
+            run_linearInterpolation()
+        case 2:
+            run_polynomialInterpolation()
+        case 3:
+            run_lagrange_interpolation()
+
